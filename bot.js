a67ef9084006e3e78705c1ee493a25fc44f33bb4
NiitoCHKN <38116459+NiitoCHKN@users.noreply.github.com>
2018-04-06 19:32:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,6 @@ bot.on('ready', () => {
 });
 
 bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find('name', 'member-log');
-  if (!channel) return;
-  channel.send(`Welcome to the server, ${member}`);
-});
   member.user.send("");
   member.user.send("");
   member.user.send("");
