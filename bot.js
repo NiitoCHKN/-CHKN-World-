8e55674a3901064bb01525aa28bb4dcd85e1c707
NiitoCHKN <38116459+NiitoCHKN@users.noreply.github.com>
2018-04-09 18:24:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,23 +10,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'Manager? La segretaria ha detto che volevi dirmi qualcosa, dimmi tutto') {
-    msg.reply('Mi sento troppo un boss perchè in due giorni mi hai insegnato più tu che i miei prof alle medie e alle superiori, ho già imparato anche il level system in così poco tempo :D');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'Mi fa piacere! Abbiamo appena iniziato eheh') {
-    msg.reply('Ottimo! ma ora devo scappare perchè ho tante cose da fare, il ruolo da manager richiede tanto lavoro! Alla prossima!');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === 'yo') {
-    msg.reply('yo');
-  }
-});
-
 client.on("guildMemberAdd", member => {
  const channel = member.guild.channels.get('204986384233136129');
   if (!channel) return;
