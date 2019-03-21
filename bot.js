72db60b71992b48ee390f398b49e2400514a858a
NiitoCHKN <38116459+NiitoCHKN@users.noreply.github.com>
2019-03-21 17:11:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,15 +17,7 @@ client.on("guildMemberAdd", member => {
 });
 
 client.on('guildMemberAdd', member => {
-  member.user.send("");
-  member.user.send("");
-  member.user.send("");
-  member.user.send("");
-  member.user.send("");
-  member.user.send("");
-  member.user.send("");
-  member.user.send("");
-  member.user.send("**● CHI SIAMO**\n  \nI **CHKN** nascono un po' per scherzo un po' per pazzia nei primi mesi del 2017. Come? Ve lo spiego subito.");
+  member.user.send("**● CHI SIAMO**\n\n\nI **CHKN** nascono un po' per scherzo un po' per pazzia nei primi mesi del 2017. Come? Ve lo spiego subito.\n\n**Niito**, che è anche la persona che sta scrivendo questo testo e che ha fondato il tutto, dopo aver acquistato Dead by Daylight nel settembre 2016 grazie ad amici di amici, conosce un ragazzo di nome **LycanCHKN**. Da come si può intuire, l'ispirazione di creare una community con questo nome, arriva proprio da lui. Dopo averlo conosciuto, continuiamo a giocare insieme, per giorni, settimane, **mesi**. Il punto di ritrovo era sempre lo stesso, il ts di PrimeSquad. Ogni giorno, dal primo pomeriggio fino al darci la buonanotte, giocavamo a Dead by Daylight. Il menù era sempre quello, ogni giorno, per mesi: **Ts->Dbd**.\n\nMentre tutto questo rendeva le nostre giornate piacevoli e divertenti, conosco **Vetto** che fin da subito ci invitò nel suo server Teamspeak. Da quel momento in poi, il menù pomeridiano subì un'inevitabile cambiamento, che segnò anche l'inizio del CHKN World e della Nitocrazia.\nDall'insediamento nel teamspeak di Vetto, comincio a conoscere tanti ragazzi, alcuni dei quali sono poi diventati pilastri di questa **community.**\nE la formula era sempre la stessa:\n**Conosci Niito**->Ti invita nel Ts->Capisci che una volta aver conosciuto Niito non puoi più **scappare** via->Continui a giocare con lui e gli altri sul suo ts **fino a diventare parte del tutto.**\nE siamo arrivati ad oggi, che siamo talmente potenti da poterci permettere anche un server discord.\n\n\n**● REGOLAMENTO**\n\nOgni community che si rispetti ha bisogno di regole per andare avanti in modo pacifico e sereno.\nMa a noi non ce ne frega proprio un cazzo, perchè tanto i regolamenti nessuno li legge, e poi le regole sono sempre le stesse, non c'è bisogno che le scrivo per fare bella figura sui nuovi che entrano, l'unica cosa che posso dirti è:\nSei davvero sicuro di volerne far parte? Ti avverto che una volta entrati, non si esce. La Niitocrazia ti rimarrà impressa per sempre.\n\n🐔🥂 Se sei sicuro, accomodati, occhio però, qui il tempo scorre veloce. 🐔🥂");
            console.log(`${member.user.username} has joined`);
 });
 
